870ba0f04fc555c15c7b4a266f780476bf6b4974
jayeshbbaviskar <jayesh.intheworld1987@gmail.com>
2020-07-29 19:33:20 +0530

    Changes required to run it at my End

--- AutoUpdaterTest/FormMain.Designer.cs
+++ AutoUpdaterTest/FormMain.Designer.cs
@@ -37,9 +37,9 @@
             this.buttonCheckForUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCheckForUpdate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCheckForUpdate.Location = new System.Drawing.Point(12, 54);
+            this.buttonCheckForUpdate.Location = new System.Drawing.Point(12, 221);
             this.buttonCheckForUpdate.Name = "buttonCheckForUpdate";
-            this.buttonCheckForUpdate.Size = new System.Drawing.Size(196, 40);
+            this.buttonCheckForUpdate.Size = new System.Drawing.Size(472, 40);
             this.buttonCheckForUpdate.TabIndex = 0;
             this.buttonCheckForUpdate.Text = "Check for update";
             this.buttonCheckForUpdate.UseVisualStyleBackColor = true;
@@ -51,15 +51,15 @@
             this.labelVersion.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelVersion.Location = new System.Drawing.Point(9, 23);
             this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(130, 15);
+            this.labelVersion.Size = new System.Drawing.Size(328, 41);
             this.labelVersion.TabIndex = 1;
             this.labelVersion.Text = "Current version : 1.0.0.0";
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(220, 106);
+            this.ClientSize = new System.Drawing.Size(584, 273);
             this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.buttonCheckForUpdate);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
